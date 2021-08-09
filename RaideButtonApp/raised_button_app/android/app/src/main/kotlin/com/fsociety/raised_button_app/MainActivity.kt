54bcc7eadd5b947e7d7d93aa6dcacdadf45d695f
LenouarMiloud <lenouarmiloud@gmail.com>
2021-08-09 17:45:47 +0200

    add new Project and screenshot

---
+++ RaideButtonApp/raised_button_app/android/app/src/main/kotlin/com/fsociety/raised_button_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fsociety.raised_button_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ RaideButtonApp/raised_button_app/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp>{
+
+  String _value = "LENOUAR Miloud";
+
+  void _onPressed(){
+    setState(() {
+      _value = "My Name is LENOUAR Miloud";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('App Bar Name here'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text(_value),
+              new RaisedButton(onPressed: _onPressed ,child: new Text("Click Me"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
